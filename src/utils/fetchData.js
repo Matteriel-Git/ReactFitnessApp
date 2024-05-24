44cfa62b97f400a249fb1c1f5641e28becc9831f
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2024-05-24 14:38:55 -0700

    Add files via upload

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -16,6 +16,9 @@ export const exerciseOptions = {
 
 export const fetchData = async (url,options) => {
     const response = await fetch(url,options);
+    if(!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
 
     return data;
