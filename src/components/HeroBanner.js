526135d0c2e296abd833fad2afe54d2cb7b08a9c
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2024-10-27 21:45:12 -0700

    Add files via upload

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Box, Stack, Typography, Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import HeroBannerImage from '../assets/images/banner.png';
 
-/* eslint-disable no-unused-vars */
 
 
 const HeroBanner = () => {
